d21882b2643f88c86ae6f0fd9c3b4cf4aba6b2f2
Eliac <deylin1616@gmail.com>
2026-06-11 17:49:56 -0600

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 pkg update && pkg upgrade -y
 pkg install nodejs git ffmpeg imagemagick libwebp -y
-npm install -g pm2
-npm install blessed
-# Ejecuta el panel para que él se encargue de todo lo demás
+npm install blessed blessed-contrib
 node panel.js
