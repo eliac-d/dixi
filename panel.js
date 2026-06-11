311b7155b58130ba322420a489a8fd132d265c15
Eliac <deylin1616@gmail.com>
2026-06-11 17:52:16 -0600

    Actualizar panel.js

--- panel.js
+++ panel.js
@@ -3,36 +3,27 @@ const { spawn } = require('child_process');
 const fs = require('fs');
 
 const screen = blessed.screen({ smartCSR: true, title: 'Dixi Dashboard' });
-const log = blessed.log({ top: 0, left: 0, width: '100%', height: '80%', border: { type: 'line' }, label: ' ESTADO ', scrollable: true, alwaysScroll: true });
+const log = blessed.log({ top: 0, left: 0, width: '100%', height: '80%', border: { type: 'line' }, label: ' PANEL DE CONTROL ', scrollable: true, alwaysScroll: true });
 const input = blessed.textbox({ bottom: 0, left: 0, width: '100%', height: 3, border: { type: 'line' }, label: ' COMANDO ', inputOnFocus: true });
 
 screen.append(log); screen.append(input);
 
-function iniciarPregunta() {
-  log.add('{yellow-fg}>> ¿Quieres instalar Kirito Bot MD? (S/N){/}');
-  input.focus();
-}
+log.add('{cyan-fg}>>> PANEL CARGADO. ESPERANDO INSTRUCCIONES...{/}');
 
 input.on('submit', (val) => {
-  const resp = val.trim().toUpperCase();
-  if (resp === 'S') {
-    log.add('{green-fg}>> Iniciando instalación de Kirito Bot MD...{/}');
-    input.clearValue();
-    const inst = spawn('bash', ['-c', 'npm install -g pm2 && git clone https://github.com/eliac-d/kirito-Bot-MD && cd kirito-Bot-MD && npm install']);
+  const cmd = val.trim();
+  if (cmd === 'exit') process.exit(0);
+  
+  if (cmd === 'instalar') {
+    log.add('{yellow-fg}>> Iniciando instalación de Kirito-Bot-MD...{/}');
+    const inst = spawn('bash', ['-c', 'git clone https://github.com/eliac-d/kirito-Bot-MD && cd kirito-Bot-MD && npm install']);
     inst.stdout.on('data', (d) => log.add(d.toString()));
-    inst.on('close', () => {
-      log.add('{green-fg}>> Instalación completa. Iniciando con PM2...{/}');
-      spawn('pm2', ['start', 'kirito-Bot-MD/index.js', '--name', 'kirito']);
-    });
-  } else if (resp === 'N') {
-    log.add('{red-fg}>> Instalación cancelada.{/}');
-    input.clearValue();
-  } else if (resp === 'EXIT') {
-    process.exit(0);
+    inst.on('close', () => log.add('{green-fg}>> Instalación finalizada.{/}'));
   } else {
-    log.add('> ' + val);
-    input.clearValue();
+    log.add('> ' + cmd);
   }
+  
+  input.clearValue();
   input.focus();
   screen.render();
 });
@@ -40,4 +31,4 @@ input.on('submit', (val) => {
 screen.key(['C-c'], () => process.exit(0));
 
 screen.render();
-setTimeout(iniciarPregunta, 500);
+input.focus();
